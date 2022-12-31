0567a3457285eb71da6083933b7d1ae609f008ec
sue445 <sue445@sue445.net>
2022-12-31 14:10:37 +0900

    bundle gem --mit rspec-parameterized-table_syntax

---
+++ lib/rspec/parameterized/table_syntax.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "table_syntax/version"
+
+module Rspec
+  module Parameterized
+    module TableSyntax
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ spec/rspec/parameterized/table_syntax_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Rspec::Parameterized::TableSyntax do
+  it "has a version number" do
+    expect(Rspec::Parameterized::TableSyntax::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
