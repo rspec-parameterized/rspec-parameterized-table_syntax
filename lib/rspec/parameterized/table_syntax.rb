2a37c9ad0cbf34dbd5e95209a7a3cbaa6c686d3e
sue445 <sue445@sue445.net>
2026-02-15 23:04:01 +0900

    Revert "Allow binding_of_caller to load under Ruby 4.0"
    This reverts commit f20df32133ca356573a454b76b9d552fb6cbc2c2.

--- lib/rspec/parameterized/table_syntax.rb
+++ lib/rspec/parameterized/table_syntax.rb
@@ -4,25 +4,7 @@ require "rspec/parameterized/table_syntax/version"
 require 'rspec/parameterized/table_syntax/table'
 require 'rspec/parameterized/table_syntax/table_syntax_implement'
 require "rspec/parameterized/core"
-
-# FIXME: Monkey patch for Ruby 4.0+
-# ref. https://github.com/hanami/hanami-webconsole/pull/12
-if RUBY_ENGINE == "ruby"
-  # Skip binding_of_caller's version check and force it to load on Ruby 4.0.
-  #
-  # Remove this shim once https://github.com/banister/binding_of_caller/pull/90 is merged and a new
-  # release has been made.
-  require "binding_of_caller/version"
-  require "binding_of_caller/mri"
-
-  # Prevent a direct require to "binding_of_caller", which is no longer needed by this point, and
-  # which prints a misleading warning about it not supporting Ruby 4.0, which we've fixed ourselves
-  # via the above.
-  $LOADED_FEATURES << "binding_of_caller.rb"
-else
-  require "binding_of_caller"
-end
-# require 'binding_of_caller'
+require 'binding_of_caller'
 
 module RSpec
   module Parameterized
